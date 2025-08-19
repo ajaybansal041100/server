e1782cf992d50dc740e5d9ba326364c8301bfcda
ajaybansal041100 <ajaybansal041100@gmail.com>
2025-08-19 11:40:20 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000
 const web = require('./routes/web')
 const connectDB=require('./db/connectDB')
 const fileuplaod=require('express-fileupload')
